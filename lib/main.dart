2bb86bad10bb8a70c0cb2b7852907d3348d8e0d0
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-16 18:25:09 +0530

    use useState to complete the rest of widget.

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,32 @@ class HooksCounter extends HookWidget {
   const HooksCounter({super.key});
   @override
   Widget build(BuildContext context) {
+    final counter = useState(0);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hooks Increment"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("You've pressed the button this many times:${counter.value}"),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            counter.value++;
+          },
+          tooltip: "Increment",
+          child: const Icon(Icons.add),
+        ),
+      )
     );
   }
 }
